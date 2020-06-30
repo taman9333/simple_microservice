8e08f68754248e99a6e3665b97807c8dda630b6b
taman9333 <taman9333@gmail.com>
2020-07-01 01:06:17 +0200

    Emitting events when post or comment created

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 const { randomBytes } = require("crypto");
 
 const app = express();
@@ -13,12 +14,21 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const comments = commentsByPostId[req.params.id] || [];
 
   comments.push({ id: commentId, content: req.body.content });
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:3000/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
 
   res.status(201).send(comments);
 });
--- posts/index.js
+++ posts/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 const { randomBytes } = require("crypto");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 const posts = {};
 
@@ -13,11 +14,17 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
   posts[id] = { id, title };
+
+  await axios.post("http://localhost:3000/events", {
+    type: "PostCreated",
+    data: { id, title },
+  });
+
   res.status(201).send(posts[id]);
 });
 
