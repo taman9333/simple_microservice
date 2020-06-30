86c7b82c5f000506b9a23b2a9d9386e02e696984
taman9333 <taman9333@gmail.com>
2020-06-30 17:40:07 +0200

    Add entry point for naive express implementation with only 2 endpoints for creating & getting posts

---
+++ posts/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/posts', (req, res) => {
+
+});
+
+app.post('/posts', (req, res) => {
+
+});
+
+app.listen('4000', () => console.log('Listening on port 4000...'));
