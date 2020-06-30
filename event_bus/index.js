59bc397d6559947ca634b5db6c7dda3f07e5705d
taman9333 <taman9333@gmail.com>
2020-07-01 00:56:15 +0200

    Implement Naive event bus :truck:

---
+++ event_bus/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  // we need to handle failure if any of these services is down
+  axios.post("http://localhost:4000/events", event);
+  axios.post("http://localhost:4001/events", event);
+  axios.post("http://localhost:4002/events", event);
+
+  res.send({ status: "ok" });
+});
+
+app.listen("3000", () => "Event-Bus started, listening on port 3000...");
