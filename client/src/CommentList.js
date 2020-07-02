4b14e223168be025821bb2d59acd5ec8d6d85d3c
taman9333 <taman9333@gmail.com>
2020-07-03 01:14:19 +0200

    Render comment in client app by its status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React from "react";
 
 export default ({ comments }) => {
-  const renderedComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.content}</li>;
+  const renderedComments = comments.map((comment) => {
+    let content;
+
+    if (comment.status === "approved") {
+      content = comment.content;
+    } else if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    } else {
+      // pending or any other state
+      content = "This comment is awaiting moderation";
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
